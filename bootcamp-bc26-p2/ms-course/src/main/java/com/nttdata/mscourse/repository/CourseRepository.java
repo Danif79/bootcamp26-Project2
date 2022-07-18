d3d5189d11144bbe39fa916d4251b31c043836f2
Danif79 <108303000+Danif79@users.noreply.github.com>
2022-07-18 11:43:39 -0500

    Delete bootcamp-bc26-p2 directory

--- bootcamp-bc26-p2/ms-course/src/main/java/com/nttdata/mscourse/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.mscourse.repository;
-
-import com.nttdata.mscourse.model.Course;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends MongoRepository<Course, String> {
-}
--- bootcamp-bc26-p2/ms-note/src/main/java/com/nttdata/msnote/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.msnote.repository;
-
-import com.nttdata.msnote.model.Note;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends MongoRepository<Note, String> {
-}
